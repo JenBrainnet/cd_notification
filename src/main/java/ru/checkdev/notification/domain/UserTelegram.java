b26a494f87eaf13d0639852c2dabe7941dd0e425
JenBrainnet <benebrak01@gmail.com>
2026-05-27 12:59:41 +0300

    Telegram Bot

--- src/main/java/ru/checkdev/notification/domain/UserTelegram.java
+++ src/main/java/ru/checkdev/notification/domain/UserTelegram.java
@@ -1,6 +1,10 @@
 package ru.checkdev.notification.domain;
 
-import jakarta.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -15,6 +19,7 @@ import lombok.NoArgsConstructor;
 public class UserTelegram implements Base {
     @EqualsAndHashCode.Include
     @Id
+    @jakarta.persistence.Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Column(name = "user_id", nullable = false, unique = true)
